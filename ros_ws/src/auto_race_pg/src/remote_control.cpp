e9c83c9b84141570352cb6a65823a0a75036c9ae
Sami <sami.ansawi@tu-dortmund.de>
2018-11-08 20:26:30 +0100

    - fine tuning - added joy node to launch

--- ros_ws/src/auto_race_pg/src/remote_control.cpp
+++ ros_ws/src/auto_race_pg/src/remote_control.cpp
@@ -5,15 +5,10 @@
 #include <termios.h>
 #include <signal.h>
 
-#define KEYCODE_A 97; 
-#define KEYCODE_S 115; 
-#define KEYCODE_D 100; 
-#define KEYCODE_W 119; 
-
-#define KEYCODE_A 97; 
-#define KEYCODE_A 97; 
-#define KEYCODE_A 97; 
-#define KEYCODE_A 97; 
+#define KEYCODE_W 119
+#define KEYCODE_A 97
+#define KEYCODE_S 115
+#define KEYCODE_D 100
 
 class RemoteControl
 {
@@ -23,24 +18,23 @@ class RemoteControl
 
     private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-    void testCallback(const geometry_msgs::Twist::ConstPtr& twist);
 
     int getch();
 
-    void adjustCar(int angle, int speed);
+    void adjustCar(float speed, float angle);
 
     ros::NodeHandle nh_;
 
     int             linear_, angular_;
     double          l_scale_, a_scale_;
-    ros::Publisher  ros_out;
-    ros::Subscriber joy_in;
-    ros::Subscriber ros_in;
+
+    ros::Publisher  out_ros;
+    ros::Subscriber in_joy;
 };
 
 RemoteControl::RemoteControl()
     : linear_(1)
-    , angular_(2)
+    , angular_(0)
 {
 
     nh_.param("axis_linear", linear_, linear_);
@@ -48,40 +42,54 @@ RemoteControl::RemoteControl()
     nh_.param("scale_angular", a_scale_, a_scale_);
     nh_.param("scale_linear", l_scale_, l_scale_);
 
-    ros_out = nh_.advertise< geometry_msgs::Twist >("turtle1/cmd_vel", 1);
+    out_ros = nh_.advertise< geometry_msgs::Twist >("turtle1/cmd_vel", 1);
 
-    joy_in =
-        nh_.subscribe< sensor_msgs::Joy >("joy", 10,
-                                          &RemoteControl::joyCallback, this);
-
-    ros_in =
-        nh_.subscribe< geometry_msgs::Twist >("turtle1/cmd_vel", 1,
-                                            &RemoteControl::testCallback, this);
+    in_joy =
+        nh_.subscribe< sensor_msgs::Joy >("joy", 10, &RemoteControl::joyCallback, this);
 }
 
 void RemoteControl::keyLoop() {
-    std::cout << "listening to keyboard" << std::endl;
-    std::cout << "=====================" << std::endl;
+    std::cout << "listening to keyboard and controller" << std::endl;
+    std::cout << "====================================" << std::endl;
     while (ros::ok())
     {
-        int c = getch();   // call your non-blocking input function
+	int c = getch();
         std::cout << c << std::endl;
-
         adjustCar(0, 0);
+	
+	float speed = 0;
+	float angle = 0;
+
+	switch(c) {
+		case KEYCODE_W:
+			speed++;
+			break;
+		case KEYCODE_S:
+			speed--;
+			break;
+		case KEYCODE_A:
+			angle--;
+			break;
+		case KEYCODE_D:
+			angle++;
+			break;
+		default:
+			break;
+	}
+	adjustCar(speed, angle);
     }
 }
 
 void RemoteControl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-    geometry_msgs::Twist twist;
-    twist.angular.z = a_scale_ * joy->axes[ angular_ ];
-    twist.linear.x  = l_scale_ * joy->axes[ linear_ ];
-    ros_out.publish(twist);
-}
+    float speed = joy->axes[ linear_ ];
+    float angle = joy->axes[ angular_ ];
+    adjustCar(speed, angle);
 
-void RemoteControl::testCallback(const geometry_msgs::Twist::ConstPtr& twist)
-{
-
+    //geometry_msgs::Twist twist;
+    //twist.angular.z = a_scale_ * joy->axes[ angular_ ];
+    //twist.linear.x  = l_scale_ * joy->axes[ linear_ ];
+    //ros_out.publish(twist);
 }
 
 int RemoteControl::getch()
@@ -98,8 +106,8 @@ int RemoteControl::getch()
   return c;
 }
 
-void RemoteControl::adjustCar(int angle, int speed) {
-
+void RemoteControl::adjustCar(float speed, float angle) {
+    std::cout << speed << "  " << angle << std::endl;
 }
 
 void quit(int sig)
@@ -111,10 +119,14 @@ void quit(int sig)
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "remove_control");
-    RemoteControl remove_control;
+    RemoteControl remote_control;
 
-    signal(SIGINT, quit);
-    remove_control.keyLoop();
+    //std::cout << "listining to keyboard and controller" << std::endl;
+    //std::cout << "====================================" << std::endl;
 
-    return 0;
+    //signal(SIGINT, quit);
+    //remote_control.keyLoop();
+    //return 0;
+
+    ros::spin();
 }
