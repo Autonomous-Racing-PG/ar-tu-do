609aa1a7a2d2ec1c90e4adc0aa0cf6e0a5ab3c87
Sami <sami.ansawi@tu-dortmund.de>
2018-11-09 13:44:00 +0100

    - topic constant - cmake removed listener and talker

--- ros_ws/src/auto_race_pg/src/remote_control.cpp
+++ ros_ws/src/auto_race_pg/src/remote_control.cpp
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <signal.h>
 
+#define CONTROL_TOPIC "turtle1/cmd_vel"
+
 #define KEYCODE_W 119
 #define KEYCODE_A 97
 #define KEYCODE_S 115
@@ -42,7 +44,7 @@ RemoteControl::RemoteControl()
     nh_.param("scale_angular", a_scale_, a_scale_);
     nh_.param("scale_linear", l_scale_, l_scale_);
 
-    out_ros = nh_.advertise< geometry_msgs::Twist >("turtle1/cmd_vel", 1);
+    out_ros = nh_.advertise< geometry_msgs::Twist >(CONTROL_TOPIC, 1);
 
     in_joy =
         nh_.subscribe< sensor_msgs::Joy >("joy", 10, &RemoteControl::joyCallback, this);
@@ -85,11 +87,6 @@ void RemoteControl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     float speed = joy->axes[ linear_ ];
     float angle = joy->axes[ angular_ ];
     adjustCar(speed, angle);
-
-    //geometry_msgs::Twist twist;
-    //twist.angular.z = a_scale_ * joy->axes[ angular_ ];
-    //twist.linear.x  = l_scale_ * joy->axes[ linear_ ];
-    //ros_out.publish(twist);
 }
 
 int RemoteControl::getch()
