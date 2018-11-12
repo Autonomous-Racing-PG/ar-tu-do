a7c7b761e74ddf9b4f65b6fedfdf4ada7b226b0d
Sami <sami.ansawi@tu-dortmund.de>
2018-11-12 19:23:54 +0100

    added missing ";"

--- ros_ws/src/auto_race_pg/src/car_control.cpp
+++ ros_ws/src/auto_race_pg/src/car_control.cpp
@@ -13,79 +13,77 @@
 
 
 class CarControl
-
-
 {
     public:
-    CarControl()
+    CarControl();
 
     private:
-    ros:: NodeHandle nh_
+    ros::NodeHandle nh_;
 
-    ros:: Subscriber  in_speed
-    ros:: Subscriber  in_angle
+    ros::Subscriber  in_speed;
+    ros::Subscriber  in_angle;
 
-    void speed_callback(const std_msgs: : Float64: : ConstPtr & speed)
-    void angle_callback(const std_msgs: : Float64: : ConstPtr & angle)
+    void speed_callback(const std_msgs::Float64::ConstPtr & speed);
+    void angle_callback(const std_msgs::Float64::ConstPtr & angle);
 
-    ros:: Publisher  out_speed
-    ros:: Publisher  out_angle
+    ros::Publisher  out_speed;
+    ros::Publisher  out_angle;
 
-    void adjustSpeed(double speed)
-    void adjustAngle(double angle)
+    void adjustSpeed(double speed);
+    void adjustAngle(double angle);
 
-    bool run
-}
+    bool run;
+};
 
-CarControl: : CarControl()
+CarControl::CarControl()
 : run(true)
 {
-    out_speed = nh_.advertise < std_msgs:: Float64 > (TOPIC_FOCBOX_SPEED, 1)
-    out_angle = nh_.advertise < std_msgs:: Float64 > (TOPIC_FOCBOX_ANGLE, 1)
+    out_speed = nh_.advertise < std_msgs::Float64 > (TOPIC_FOCBOX_SPEED, 1);
+    out_angle = nh_.advertise < std_msgs::Float64 > (TOPIC_FOCBOX_ANGLE, 1);
 
     in_speed =
-    nh_.subscribe < std_msgs:: Float64 > (TOPIC_SPEED, 1, & CarControl: : speed_callback, this)
+    nh_.subscribe < std_msgs:: Float64 > (TOPIC_SPEED, 1, & CarControl::speed_callback, this);
     in_angle =
-    nh_.subscribe < std_msgs:: Float64 > (TOPIC_ANGLE, 1, & CarControl: : angle_callback, this)
+    nh_.subscribe < std_msgs:: Float64 > (TOPIC_ANGLE, 1, & CarControl::angle_callback, this);
 }
 
-void CarControl:: speed_callback(const std_msgs: : Float64: : ConstPtr & speed) {
-    adjustSpeed(speed -> data)
+void CarControl::speed_callback(const std_msgs::Float64::ConstPtr & speed) {
+    adjustSpeed(speed -> data);
 }
 
-void CarControl:: angle_callback(const std_msgs: : Float64: : ConstPtr & angle) {
-    adjustAngle(angle -> data)
+void CarControl::angle_callback(const std_msgs::Float64::ConstPtr & angle) {
+    adjustAngle(angle -> data);
 }
 
 
-void CarControl: : adjustSpeed(double speed) {
+void CarControl:: adjustSpeed(double speed) {
     if(!run) {
-        std:: cout << "fail safe" << std: : endl
+        std::cout << "fail safe" << std::endl;
     }
     else {
-        std:: cout << "speed: " << speed << std: : endl
-        std_msgs:: Float64 msg
-        msg.data = speed * MAX_SPEED
-        out_speed.publish(msg)
+        std::cout << "speed: " << speed << std::endl;
+        std_msgs::Float64 msg;
+        msg.data = speed * MAX_SPEED;
+        out_speed.publish(msg);
     }
 }
 
-void CarControl: : adjustAngle(double angle) {
+void CarControl::adjustAngle(double angle) {
     if(!run) {
-        std:: cout << "fail safe" << std: : endl
+        std::cout << "fail safe" << std::endl;
     }
     else {
-        std:: cout << "angle: " << angle << std: : endl
-        std_msgs:: Float64 msg
-        msg.data = (-angle * MAX_ANGLE + 1) / 2
-        out_angle.publish(msg)
+        std::cout << "angle: " << angle << std::endl;
+        std_msgs::Float64 msg;
+        msg.data = (-angle * MAX_ANGLE + 1) / 2;
+        out_angle.publish(msg);
     }
 }
 
 int main(int argc, char ** argv)
 {
-    ros:: init(argc, argv, "car_control")
-    CarControl car_control
+    ros::init(argc, argv, "car_control");
+    CarControl car_control;
 
-    ros:: spin()
+    ros::spin();
 }
--- ros_ws/src/auto_race_pg/src/remote_joy.cpp
+++ ros_ws/src/auto_race_pg/src/remote_joy.cpp
@@ -11,63 +11,61 @@
 
 
 class RemoteJoy
-
-
 {
     public:
-    RemoteJoy()
-    void keyLoop()
+    RemoteJoy();
+    void keyLoop();
 
     private:
-    void joyCallback(const sensor_msgs:: Joy: : ConstPtr & joy)
+    void joyCallback(const sensor_msgs::Joy::ConstPtr & joy);
 
-    void adjustAngle(double angle)
-    void adjustSpeed(double speed)
+    void adjustAngle(double angle);
+    void adjustSpeed(double speed);
 
-    ros: : NodeHandle nh_
+    ros::NodeHandle nh_;
 
-    std: : string input
+    std::string input;
 
-    ros: : Publisher  out_speed
-    ros: : Publisher  out_angle
+    ros::Publisher  out_speed;
+    ros::Publisher  out_angle;
 
-    ros: : Subscriber in_joy
-}
+    ros::Subscriber in_joy;
+};
 
-RemoteJoy:: RemoteJoy()
+RemoteJoy::RemoteJoy()
 {
-    out_speed = nh_.advertise < std_msgs: : Float64 > (TOPIC_SPEED, 1)
-    out_angle = nh_.advertise < std_msgs: : Float64 > (TOPIC_ANGLE, 1)
+    out_speed = nh_.advertise < std_msgs::Float64 > (TOPIC_SPEED, 1);
+    out_angle = nh_.advertise < std_msgs::Float64 > (TOPIC_ANGLE, 1);
 
     in_joy =
-    nh_.subscribe < sensor_msgs: : Joy > ("joy", 10, & RemoteJoy: : joyCallback, this)
+    nh_.subscribe < sensor_msgs::Joy > ("joy", 10, & RemoteJoy::joyCallback, this);
 }
 
-void RemoteJoy: : joyCallback(const sensor_msgs: : Joy: : ConstPtr & joy)
+void RemoteJoy::joyCallback(const sensor_msgs::Joy::ConstPtr & joy)
 {
-    double angle = joy -> axes[JOY_ANGLE_ANGULAR]
-    double speed = joy -> axes[JOY_ANGLE_LINEAR]
-    adjustSpeed(speed)
-    adjustAngle(angle)
+    double angle = joy -> axes[JOY_ANGLE_ANGULAR];
+    double speed = joy -> axes[JOY_ANGLE_LINEAR];
+    adjustSpeed(speed);
+    adjustAngle(angle);
 }
 
-void RemoteJoy:: adjustAngle(double angle) {
-    std_msgs: : Float64 msg
-    msg.data = (angle + 1) / 2
-    out_angle.publish(msg)
+void RemoteJoy::adjustAngle(double angle) {
+    std_msgs::Float64 msg;
+    msg.data = (angle + 1) / 2;
+    out_angle.publish(msg);
 }
 
-void RemoteJoy:: adjustSpeed(double speed) {
-    std_msgs: : Float64 msg
-    msg.data = speed
-    out_speed.publish(msg)
+void RemoteJoy::adjustSpeed(double speed) {
+    std_msgs::Float64 msg;
+    msg.data = speed;
+    out_speed.publish(msg);
 }
 
 
 int main(int argc, char ** argv)
 {
-    ros: : init(argc, argv, "remove_joy")
-    RemoteJoy remote_joy
+    ros::init(argc, argv, "remove_joy");
+    RemoteJoy remote_joy;
 
-    ros: : spin()
+    ros::spin();
 }
