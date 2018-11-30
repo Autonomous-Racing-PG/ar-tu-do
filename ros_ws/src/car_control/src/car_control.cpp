6f419c935180c2115e8ee66728acf5e9007a5b95
Sami <sami.ansawi@tu-dortmund.de>
2018-11-30 15:01:09 +0100

    emergencstop
    implemented emergency-stop

--- ros_ws/src/car_control/src/car_control.cpp
+++ ros_ws/src/car_control/src/car_control.cpp
@@ -5,8 +5,20 @@ CarControl::CarControl()
     , speed{ 0 }
     , angle{ 0 }
 {
+<<<<<<< master:ros_ws/src/car_control/src/car_control.cpp
     in_drive_param = nh_.subscribe< drive_msgs::drive_param >(
         TOPIC_DRIVE_PARAM, 1, &CarControl::drive_param_callback, this);
+=======
+    in_speed =
+        nh_.subscribe< std_msgs::Float64 >(TOPIC_SPEED, 1,
+                                           &CarControl::speed_callback, this);
+    in_angle =
+        nh_.subscribe< std_msgs::Float64 >(TOPIC_ANGLE, 1,
+                                           &CarControl::angle_callback, this);
+    in_cmd =
+        nh_.subscribe< std_msgs::String >(TOPIC_CMD, 1,
+                                           &CarControl::cmd_callback, this);
+>>>>>>> emergencstop:ros_ws/src/auto_race_pg/src/car_control.cpp
 
     out_speed = nh_.advertise< std_msgs::Float64 >(TOPIC_FOCBOX_SPEED, 1);
     out_angle = nh_.advertise< std_msgs::Float64 >(TOPIC_FOCBOX_ANGLE, 1);
@@ -20,7 +32,28 @@ void CarControl::drive_param_callback(
 
 void CarControl::adjustDriveParam(double raw_speed, double raw_angle)
 {
+<<<<<<< master:ros_ws/src/car_control/src/car_control.cpp
     speed = raw_speed * MAX_SPEED;
+=======
+    adjustAngle(angle->data);
+}
+void CarControl::cmd_callback(const std_msgs::String::ConstPtr& cmd)
+{
+    std::string str = cmd->data;
+    if(str.compare("stop") == 0) {
+        run = false;
+        adjustSpeed(0);
+        adjustAngle(0);
+    }
+    if(str.compare("go") == 0) {
+        run = true;
+    }
+}
+
+void CarControl::adjustSpeed(double raw)
+{
+    speed = raw * MAX_SPEED;
+>>>>>>> emergencstop:ros_ws/src/auto_race_pg/src/car_control.cpp
     if (speed < MIN_SPEED)
     {
         speed = 0;
