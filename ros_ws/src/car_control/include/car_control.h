12ec5729cbd48528cf567003f4919af9a047e81d
Nils <nilsjung42@gmail.com>
2018-12-06 17:18:33 +0100

    Fix pull request #85

--- ros_ws/src/car_control/include/car_control.h
+++ ros_ws/src/car_control/include/car_control.h
@@ -6,15 +6,13 @@
 
 #include <drive_msgs/drive_param.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/String.h>
 
 #define TOPIC_FOCBOX_SPEED "/commands/motor/speed"
 #define TOPIC_FOCBOX_ANGLE "/commands/servo/position"
 
 #define TOPIC_DRIVE_PARAM "/set/drive_param"
 #define TOPIC_CMD "/cc/cmd"
-
-#define TOPIC_SPEED "/set/speed"
-#define TOPIC_ANGLE "/set/angle"
 
 #define MAX_SPEED 15000
 #define MIN_SPEED 500
@@ -29,15 +27,9 @@ class CarControl
     ros::NodeHandle nh_;
 
     ros::Subscriber in_drive_param;
-
-    void drive_param_callback(const drive_msgs::drive_param::ConstPtr& param);
-	
-    ros::Subscriber in_speed;
-    ros::Subscriber in_angle;
     ros::Subscriber in_cmd;
 
-    void speed_callback(const std_msgs::Float64::ConstPtr& speed);
-    void angle_callback(const std_msgs::Float64::ConstPtr& angle);
+    void drive_param_callback(const drive_msgs::drive_param::ConstPtr& param);
     void cmd_callback(const std_msgs::String::ConstPtr& cmd);
 
     ros::Publisher out_speed;
--- ros_ws/src/car_control/src/car_control.cpp
+++ ros_ws/src/car_control/src/car_control.cpp
@@ -7,15 +7,8 @@ CarControl::CarControl()
 {
     in_drive_param = nh_.subscribe< drive_msgs::drive_param >(
         TOPIC_DRIVE_PARAM, 1, &CarControl::drive_param_callback, this);
-    in_speed =
-        nh_.subscribe< std_msgs::Float64 >(TOPIC_SPEED, 1,
-                                           &CarControl::speed_callback, this);
-    in_angle =
-        nh_.subscribe< std_msgs::Float64 >(TOPIC_ANGLE, 1,
-                                           &CarControl::angle_callback, this);
-    in_cmd =
-        nh_.subscribe< std_msgs::String >(TOPIC_CMD, 1,
-                                           &CarControl::cmd_callback, this);
+    in_cmd = nh_.subscribe< std_msgs::String >(TOPIC_CMD, 1,
+                                               &CarControl::cmd_callback, this);
 
     out_speed = nh_.advertise< std_msgs::Float64 >(TOPIC_FOCBOX_SPEED, 1);
     out_angle = nh_.advertise< std_msgs::Float64 >(TOPIC_FOCBOX_ANGLE, 1);
@@ -30,47 +23,34 @@ void CarControl::drive_param_callback(
 void CarControl::adjustDriveParam(double raw_speed, double raw_angle)
 {
     speed = raw_speed * MAX_SPEED;
-    adjustAngle(angle->data);
-}
-void CarControl::cmd_callback(const std_msgs::String::ConstPtr& cmd)
-{
-    std::string str = cmd->data;
-    if(str.compare("stop") == 0) {
-        run = false;
-        adjustSpeed(0);
-        adjustAngle(0);
-    }
-    if(str.compare("go") == 0) {
-        run = true;
-    }
-}
-
-void CarControl::adjustSpeed(double raw)
-{
-    speed = raw * MAX_SPEED;
     if (speed < MIN_SPEED)
     {
         speed = 0;
     }
     angle = (raw_angle * MAX_ANGLE + 1) / 2;
+    std::cout << "speed: " << speed << " | angle: " << angle << std::endl;
     if (run)
     {
-    std::cout << "speed: " << speed << " | angle: " << angle << std::endl;
         std_msgs::Float64 msg_speed;
         msg_speed.data = speed;
         out_speed.publish(msg_speed);
-	}
-}
-
-void CarControl::adjustAngle(double raw)
-{
-    angle = (raw * MAX_ANGLE + 1) / 2;
-    if (run)
-    std::cout << "speed: " << speed << " | angle: " << angle << std::endl;
-    {
         std_msgs::Float64 msg;
         msg.data = angle;
         out_angle.publish(msg);
+    }
+}
+
+void CarControl::cmd_callback(const std_msgs::String::ConstPtr& cmd)
+{
+    std::string str = cmd->data;
+    if (str.compare("stop") == 0)
+    {
+        run = false;
+        adjustDriveParam(0, 0);
+    }
+    if (str.compare("go") == 0)
+    {
+        run = true;
     }
 }
 
