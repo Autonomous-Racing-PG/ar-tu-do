663d2f35149a063166a9c7c3f9fe53643cfbdaaa
Sami <sami.ansawi@tu-dortmund.de>
2018-12-12 16:04:30 +0100

    Auto stash before rebase of "master"
    implemented (dead mans switch)_controller

---
+++ ros_ws/src/car_control/include/dms_controller.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <ros/ros.h>
+
+#include <std_msgs/String.h>
+#include <std_msgs/Int64.h>
+
+#define TOPIC_COMMAND "/command"
+#define TOPIC_DMS "/set/dms"
+
+#define DMS_CHECK_RATE 20 // in Hz
+#define DMS_EXPIRATION 100 // in ms
+
+class DMSController
+{
+    public:
+    DMSController();
+	void checkDMS();
+
+    private:
+	long last_dms_message_received;
+	bool running;
+    ros::NodeHandle node_handle;
+	ros::Subscriber dms_subscriber;
+    ros::Publisher command_pulisher;
+    void dmsCallback(const std_msgs::Int64::ConstPtr& dms_message);
+};
