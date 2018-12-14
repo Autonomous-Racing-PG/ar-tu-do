30aa8347d46a781da4a5e66b66f197ed8518b196
Stefan <nafets04@gmail.com>
2018-12-14 20:42:27 +0100

    dead mans switch joystick_controller

--- ros_ws/src/teleoperation/include/joystick_controller.h
+++ ros_ws/src/teleoperation/include/joystick_controller.h
@@ -7,12 +7,13 @@
 
 #include <std_msgs/Int64.h>
 
-#define JOYSTICK_AXIS_STEERING 0
-#define JOYSTICK_AXIS_THROTTLE 5
-#define JOYSTICK_BUTTON_DEADMANSSWITCH 0
+constexpr unsigned JOYSTICK_AXIS_STEERING = 0;
+constexpr unsigned JOYSTICK_AXIS_THROTTLE = 5;
+constexpr unsigned JOYSTICK_BUTTON_DEADMANSSWITCH = 0;
 
 #define TOPIC_DRIVE_PARAMETERS "/set/drive_param"
-#define TOPIC_DMS "/set/dms"
+#define TOPIC_DEAD_MANS_SWITCH "/set/dms"
+#define TOPIC_JOY "/joy"
 
 class JoystickController
 {
@@ -21,10 +22,17 @@ class JoystickController
 
     private:
     ros::NodeHandle node_handle;
-    ros::Publisher drive_parameter_publisher;
-    ros::Publisher dms_publisher;
-    ros::Subscriber joystick_subscriber;
+
+    ros::Publisher m_drive_parameter_publisher;
+    ros::Publisher m_dead_mans_switch_publisher;
+
+    ros::Subscriber m_joystick_subscriber;
+
+    ros::Timer m_joystick_connection_timer;
+
+    bool m_joystick_connected;
 
     void joystickCallback(const sensor_msgs::Joy::ConstPtr& joystick);
     void publishDriveParameters(double velocity, double steering_angle);
+    void joystickConnectionCallback(const ros::TimerEvent& event);
 };
