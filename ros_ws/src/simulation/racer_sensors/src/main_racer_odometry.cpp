e75eaddee3c26d4df6c15505b39a472ab0b5c849
Nils <nilsjung42@gmail.com>
2019-01-14 18:47:42 +0100

    Removed racer_sensors package

--- ros_ws/src/simulation/racer_sensors/src/main_racer_odometry.cpp
+++
@@ -1,9 +0,0 @@
-#include "racer_odometry.h"
-
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "racer_odometry");
-    RacerOdometry racerOdometry;
-    ros::spin();
-    return EXIT_SUCCESS;
-}
