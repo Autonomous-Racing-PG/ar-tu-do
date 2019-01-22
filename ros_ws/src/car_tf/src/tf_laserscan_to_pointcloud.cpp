6f2d2d1f817b40b026f15966b1568a0632c9a14f
Lucas <lucas.weisse@web.de>
2019-01-22 17:34:00 +0100

    Transformation of LaserFrame(Hokuyo) to BaseFrame.
    -Transformation LaserFrame to BaseFrame
    -Transformation "sensor_msgs/LaserScan" to "sensor_msgs/PointCloud2"

--- ros_ws/src/car_tf/src/tf_laserscan_to_pointcloud.cpp
+++ ros_ws/src/car_tf/src/tf_laserscan_to_pointcloud.cpp
@@ -5,24 +5,24 @@
 class LaserscanTransformer {
      public:
         LaserscanTransformer();
-        void scanCallback(const sensor_msgs::LaserScan::ConstPtr& laserscan);
+        void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
      private:
         ros::NodeHandle node;
         laser_geometry::LaserProjection projector;
         tf::TransformListener listener;
-        ros::Publisher pointcloud_publisher;
-        ros::Subscriber laserscan_subscriber;
+        ros::Publisher point_cloud_publisher;
+        ros::Subscriber scan_subscriber;
 };
 
 LaserscanTransformer::LaserscanTransformer(){
-        laserscan_subscriber = node.subscribe<sensor_msgs::LaserScan> ("/racer/laser/scan", 100, &LaserscanTransformer::scanCallback, this);
-        pointcloud_publisher = node.advertise<sensor_msgs::PointCloud2> ("/racer/laser/tf_pointcloud", 100, false);
+        scan_subscriber = node.subscribe<sensor_msgs::LaserScan> ("/racer/laser/scan", 100, &LaserscanTransformer::scanCallback, this);
+        point_cloud_publisher = node.advertise<sensor_msgs::PointCloud2> ("/cloud", 100, false);
 }
 
-void LaserscanTransformer::scanCallback(const sensor_msgs::LaserScan::ConstPtr& laserscan){
-    sensor_msgs::PointCloud2 pointcloud;
-    projector.transformLaserScanToPointCloud("base_link", *laserscan, pointcloud, listener);
-    pointcloud_publisher.publish(pointcloud);
+void LaserscanTransformer::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+    sensor_msgs::PointCloud2 cloud;
+    projector.transformLaserScanToPointCloud("base_link", *scan, cloud, listener);
+    point_cloud_publisher.publish(cloud);
 }
 
 int main(int argc, char** argv)
