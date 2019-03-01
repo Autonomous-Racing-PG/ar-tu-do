8a0815dc929066e89977b8ec3c1bd264ed6717b8
Marian <marian.kleineberg@tu-dortmund.de>
2019-03-01 21:59:36 +0100

    Make car_config.h more readable
    fixes #169

--- ros_ws/src/simulation/vesc_sim/include/car_config.h
+++ ros_ws/src/simulation/vesc_sim/include/car_config.h
@@ -3,130 +3,92 @@
 namespace car_config
 {
     /**
-     * @brief It is not possible to use <cmath> PI definition in constexpr.
-     * That is why it is defined here again.
+     * @brief Since it's not possible to use <cmath> in a constexpression, it's defined here again.
      */
-    constexpr double PI{ 3.14159265358979323846 };
+    constexpr double PI = 3.14159265358979323846;
+
+    constexpr double DEG_TO_RAD = PI / 180;
 
     /**
-     * @brief Distance between front and rear axis, wheelbase
-     * @unit m
+     * @brief The distance between the front and rear axes
      */
-    constexpr double WHEELBASE{ 0.325 };
+    constexpr double WHEELBASE = 0.325;
+
+    constexpr double WHEEL_DIAMETER = 0.098;
+
+    constexpr double WHEEL_WIDTH = 0.042;
+
+    constexpr double FRONT_WHEEL_DISTANCE = 0.23;
+
+    constexpr double REAR_WHEEL_DISTANCE = 0.233;
+
+    constexpr double WHEEL_PERIMETER = WHEEL_DIAMETER * PI;
+
+    constexpr double TURNING_RADIUS = 0.605;
 
     /**
-     * @brief Distance between center of rear wheels suspension
-     * @unit m
-     */
-    constexpr double REAR_WHEEL_DISTANCE{ 0.233 };
-
-    /**
-     * @brief Diameter of all wheels
-     * @unit m
-     */
-    constexpr double WHEEL_DIAMETER{ 0.098 };
-
-    /**
-     * @brief Width of all wheels
-     * @unit m
-     */
-    constexpr double WHEEL_WIDTH{ 0.042 };
-
-    /**
-     * @brief Distance between center of front wheels suspension
-     * @unit m
-     */
-    constexpr double FRONT_WHEEL_DISTANCE{ 0.23 };
-
-    /**
-     * @brief Perimeter of all wheels
-     * @unit m
-     */
-    constexpr double WHEEL_PERIMETER{ WHEEL_DIAMETER * PI };
-
-    /**
-     * @brief Minimal turning radius of car
-     * @unit m
-     */
-    constexpr double TURNING_RADIUS{ 0.605 };
-
-    /**
-     * @brief revolutions per miunte of motor, divide through transmission to get wheel rpm
+     * @brief Maximum revolutions per minute of the motor. Divide by TRANSMISSION to get the maximum wheel rpm
      * @unit 1/min
      */
-    constexpr double MAX_RPM_MECHANICAL{ 60000 };
+    constexpr double MAX_RPM_MECHANICAL = 60000;
 
     /**
-     * @brief number of electrical motor poles
-     * @unit none
+     * @brief Number of electrical motor poles
      */
-    constexpr double MOTOR_POLES{ 3 };
+    constexpr double MOTOR_POLES = 3;
 
     /**
-     * @brief vesc calculates in electrical revolutions per minute
+     * @brief Maximum electrical revolutions per minute for use in the VESC
      * @unit 1/min
      */
-    constexpr double MAX_RPM_ELECTRICAL{ MAX_RPM_MECHANICAL / MOTOR_POLES };
+    constexpr double MAX_RPM_ELECTRICAL = MAX_RPM_MECHANICAL / MOTOR_POLES;
 
     /**
-     * @brief conversion gain from electrical revolutions per minute to meter per second
-     * @unit m/s * minute
+     * @brief Conversion factor from electrical revolutions per minute to meters per second
+     * @unit m/s * minute = m
      */
-    constexpr double ERPM_TO_SPEED{ WHEEL_PERIMETER * MOTOR_POLES / 60 }; // 60 seconds per minute
+    constexpr double ERPM_TO_SPEED = WHEEL_PERIMETER * MOTOR_POLES / 60;
 
     /**
-     * @brief conversion gain from meter per second to electrical revolutions per minute
-     * @unit s/ (m * minute)
+     * @brief Conversion factor from meters per second to electrical revolutions per minute
+     * @unit s/ (m * minute) = 1/m
      */
-    constexpr double SPEED_TO_ERPM{ 1 / ERPM_TO_SPEED };
+    constexpr double SPEED_TO_ERPM = 1 / ERPM_TO_SPEED;
 
     /**
-     * @brief conversion gain from mechanical revolutions per minute to meter per second
-     * @unit m/s * minute
+     * @brief Conversion factor from mechanical revolutions per minute to meters per second
+     * @unit m/s * minute = m
      */
-    constexpr double RPM_TO_SPEED{ WHEEL_PERIMETER / 60 };
+    constexpr double RPM_TO_SPEED = WHEEL_PERIMETER / 60;
 
     /**
-     * @brief offset of the servo for normal wheel position
-     * @unit none
+     * @brief Position of the servo for normal wheel position
      */
-    constexpr double STEERING_TO_SERVO_OFFSET{ 0.5 };
+    constexpr double STEERING_TO_SERVO_OFFSET = 0.5;
 
     /**
-     * @brief conversion gain from steering angle to servo input
+     * @brief Conversion factor from steering angle to servo input
      * @unit 1/radian
      */
-    constexpr double STEERING_TO_SERVO_GAIN{ -PI / 3 };
+    constexpr double STEERING_TO_SERVO_GAIN = -PI / 3;
+
+    constexpr double MAX_STEERING_ANGLE = 30 * DEG_TO_RAD;
+
+    constexpr double MIN_STEERING_ANGLE = -30 * DEG_TO_RAD;
 
     /**
-     * @brief
-     * @unit radian
+     * @brief Gear transmission inside the differential of the car.
+     * This is an estimate, the exact value is not known.
      */
-    constexpr double MAX_STEERING_ANGLE{ PI / 6 }; // 30°
+    constexpr double TRANSMISSION = 20;
+
+    constexpr double MAX_SERVO_POSITION = 1;
 
     /**
-     * @brief
-     * @unit radian
+     * @brief Conversion factor from electrical revolutions per minute to radian to seconds
+     * @unit radian/s * minute = radians
      */
-    constexpr double MIN_STEERING_ANGLE{ -PI / 6 }; //-30°
-
-    /**
-     * @brief gear transmission inside the differential of the car
-     * @unit none
-     */
-    constexpr double TRANSMISSION{ 20 }; // do not know exactly
-
-    /**
-     * @brief
-     * @unit none
-     */
-    constexpr double MAX_SERVO_POSITION{ 1 };
-
-    /**
-     * @brief conversion gain from electrical revolutions per minute to radian to seconds
-     * @unit radian/s * minute
-     */
-    constexpr double ERPM_TO_RAD_PER_SEC{ MOTOR_POLES * 2 * PI / 60 }; // 60 seconds per minute
+    constexpr double ERPM_TO_RAD_PER_SEC = MOTOR_POLES * 2 * PI / 60;
 
     constexpr char COMMAND_POSITION[]{ "/commands/servo/position" };
     constexpr char COMMAND_THROTTLE[]{ "/commands/motor/speed" };
