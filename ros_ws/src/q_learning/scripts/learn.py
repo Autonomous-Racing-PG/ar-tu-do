62713d8c406aff138aaf8417b839909599b5fa2c
Marian <marian.kleineberg@tu-dortmund.de>
2019-03-22 23:00:12 +0100

    Create a q_learning package and implement resetting the gazebo model

---
+++ ros_ws/src/q_learning/scripts/learn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from gazebo_msgs.srv import SetModelState
+from gazebo_msgs.msg import ModelState
+
+set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+
+def respawn():
+    state = ModelState()
+    state.model_name = "racer"
+    state.pose.position.x = 0
+    state.pose.position.y = 0
+    state.pose.position.z = 0
+    state.twist.linear.x = 0
+    state.twist.linear.y = 0
+    state.twist.linear.z = 0
+
+    set_model_state(state)
+
+rospy.init_node('learn', anonymous=True)
+rate = rospy.Rate(0.3)
+while not rospy.is_shutdown():
+    respawn()
+    rate.sleep()
