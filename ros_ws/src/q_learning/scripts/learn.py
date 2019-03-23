b721a51b88e0dc05a0235f8b806269de2ec3a330
Marian <marian.kleineberg@tu-dortmund.de>
2019-03-23 19:22:42 +0100

    Reset the car when a crash is detected

--- ros_ws/src/q_learning/scripts/learn.py
+++ ros_ws/src/q_learning/scripts/learn.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Empty
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.msg import ModelState
 
 rospy.wait_for_service('/gazebo/set_model_state')
 set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+
 
 def respawn():
     state = ModelState()
@@ -20,8 +21,13 @@ def respawn():
 
     set_model_state(state)
 
+def crash():
+    rospy.loginfo("Crashed.")
+    respawn()
+
+
 rospy.init_node('learn', anonymous=True)
+rospy.Subscriber("/crash", Empty, crash)
 rate = rospy.Rate(0.3)
 while not rospy.is_shutdown():
-    respawn()
     rate.sleep()
