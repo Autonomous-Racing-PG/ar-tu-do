3624958d7e1303f28e961d5203398247c6ecfb14
Nils <nilsjung42@gmail.com>
2019-04-06 13:46:15 +0200

    Combine kinetic & melodic

---
+++ ros_ws/src/simulation/racer_description/config/cartographer_config_kinetic.lua
@@ -0,0 +1,38 @@
+-- Copyright 2016 The Cartographer Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+include "map_builder.lua"
+include "trajectory_builder.lua"
+
+options = {
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
+  map_frame = "map",
+  tracking_frame = "imu_link",
+  published_frame = "base_link",
+  odom_frame = "odom",
+  provide_odom_frame = false,
+  use_odometry = false,
+  use_laser_scan = true,
+  use_multi_echo_laser_scan = false,
+  num_point_clouds = 0,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+  trajectory_publish_period_sec = 30e-3,
+}
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+
+return options
