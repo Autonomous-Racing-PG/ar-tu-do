f8701c3149afd24265e308e5da04678f654e9361
Nils <nilsjung42@gmail.com>
2019-04-07 23:24:48 +0200

    Add launch and script files for map creation

---
+++ scripts/cartographer_bag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#   Cartographer Bag Script
+#   Must be run in the ros.package directory
+#
+currentdir=$(pwd)
+echo -n "Creating .urdf file..."
+xacro -o $currentdir/ros_ws/src/navigation_stack/car_cartographer/files/racer.urdf --inorder $currentdir/ros_ws/src/simulation/racer_description/urdf/racer.xacro use_gpu:=true visualize_lidar:=true >/dev/null 2>/dev/null
+echo " Done."
+echo -n "Create .pbstream..."
+roslaunch car_cartographer cartographer_bag_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
+echo " Done."
+echo -n "Create map..."
+roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$1 pose_graph_filename:=$1.pbstream >/dev/null 2>/dev/null
+echo " Done."
---
+++ scripts/cartographer_online.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#   Cartographer Online Script
+#   Must be run in the ros.package directory
+#
+currentdir=$(pwd)
+echo -n "Creating .urdf file..."
+xacro -o $currentdir/ros_ws/src/navigation_stack/car_cartographer/files/racer.urdf --inorder $currentdir/ros_ws/src/simulation/racer_description/urdf/racer.xacro use_gpu:=true visualize_lidar:=true >/dev/null 2>/dev/null
+echo " Done."
+echo "Recording topics..."
+rosbag record -j -O ros_ws/src/navigation_stack/car_cartographer/files/recording.bag scan imu __name:=rosbag_recording >/dev/null 2>/dev/null &
+echo "Please drive with the car now (1.5 laps minimum, 3-4 laps recommended)"
+read -p "Press RETURN to stop recording"
+rosnode kill /rosbag_recording >/dev/null
+wait
+echo "Stopped recording. Done."
+echo -n "Create .pbstream..."
+roslaunch car_cartographer cartographer_bag_fast.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
+echo " Done."
+echo -n "Create map..."
+roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag pose_graph_filename:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag.pbstream >/dev/null 2>/dev/null
+echo " Done."
