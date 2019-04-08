a97605884a9c8a1e99ebebc3f5341c64510e9379
Nils <nilsjung42@gmail.com>
2019-04-08 13:44:21 +0200

    Fix melodic/kinetic difference

--- scripts/cartographer_offline.sh
+++ scripts/cartographer_offline.sh
@@ -7,9 +7,16 @@ currentdir=$(pwd)
 echo -n "Creating .urdf file..."
 xacro -o $currentdir/ros_ws/src/navigation_stack/car_cartographer/files/racer.urdf --inorder $currentdir/ros_ws/src/simulation/racer_description/urdf/racer.xacro use_gpu:=true visualize_lidar:=true >/dev/null 2>/dev/null
 echo " Done."
-echo -n "Create .pbstream..."
-roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
-echo " Done."
-echo -n "Create map..."
-roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$1 pose_graph_filename:=$1.pbstream >/dev/null 2>/dev/null
-echo " Done."
+if [$ROS_DISTRO == "kinetic"] ; then
+    echo -n "Create map..."
+    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
+    echo " Done."
+fi
+if [$ROS_DISTRO == "melodic"] ; then
+    echo -n "Create .pbstream..."
+    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
+    echo " Done."
+    echo -n "Create map..."
+    roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$1 pose_graph_filename:=$1.pbstream >/dev/null 2>/dev/null
+    echo " Done."
+fi
