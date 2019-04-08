1723abc1a686db50ed230ca588fcc5709519d8cb
Nils <nilsjung42@gmail.com>
2019-04-08 13:44:21 +0200

    Fix melodic/kinetic difference

--- scripts/cartographer_online.sh
+++ scripts/cartographer_online.sh
@@ -14,9 +14,16 @@ read -p "Press RETURN to stop recording."
 rosnode kill /rosbag_recording >/dev/null
 wait
 echo "Stopped recording. Done."
-echo -n "Create .pbstream..."
-roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
-echo " Done."
-echo -n "Create map..."
-roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag pose_graph_filename:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag.pbstream >/dev/null 2>/dev/null
-echo " Done."
+if [$ROS_DISTRO == "kinetic"] ; then
+    echo -n "Create map..."
+    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
+    echo " Done."
+fi
+if [$ROS_DISTRO == "melodic"] ; then
+    echo -n "Create .pbstream..."
+    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
+    echo " Done."
+    echo -n "Create map..."
+    roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag pose_graph_filename:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag.pbstream >/dev/null 2>/dev/null
+    echo " Done."
+fi
