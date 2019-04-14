0d2224bbd9648b4d5efeae7235b4d14af90134b6
Malik <malik.atamne@tu-dortmund.de>
2019-04-14 16:18:04 +0200

    Added Asserts, changed atan to std::atan and renamed min_dist to min_range.

--- ros_ws/src/autonomous/src/emergency_stop.cpp
+++ ros_ws/src/autonomous/src/emergency_stop.cpp
@@ -9,11 +9,16 @@ EmergencyStop::EmergencyStop()
 
 bool EmergencyStop::emergencyStop(const sensor_msgs::LaserScan::ConstPtr& lidar)
 {
+    ROS_ASSERT_MSG(RANGE_THRESHOLD <= MAX_RANGE, "Threshold is bigger then max range. Function will always return false.");
+    ROS_ASSERT_MSG(MAX_RANGE > 0, "Max range is zero or below. Function will mostly return true.");
+    ROS_ASSERT_MSG(RANGE_THRESHOLD > 0, "Threshold is zero or below. Function will mostly return false.");
+
     int available_samples = (lidar->angle_max - lidar->angle_min) / lidar->angle_increment;
 
     // calculate the sample_angle of the lidar to be used to check if there is
     // an obstacle in front of the car, with respect to the RANGE_THRESHOLD
-    float sample_angle = atan(CAR_BUMPER_LENGTH / 2 / RANGE_THRESHOLD);
+    // We want to check every lidar sample that is located in front of the car at a distance of RANGE_THRESHOLD.
+    float sample_angle = std::atan(CAR_BUMPER_LENGTH / 2 / RANGE_THRESHOLD);
 
     int index_start = available_samples / 2 - sample_angle / lidar->angle_increment / 2;
     int index_end = available_samples / 2 + sample_angle / lidar->angle_increment / 2;
@@ -24,9 +29,10 @@ bool EmergencyStop::emergencyStop(const sensor_msgs::LaserScan::ConstPtr& lidar)
     // Instead of calculating the range average, we are more cautious because we would 
     // rather have false positives instead of false negatives.
     // i.e. we would rather stop too much than crash into an obstacle.
-    auto min_dist = std::min(MAX_RANGE, *std::min_element(lidar->ranges.begin() + index_start, lidar->ranges.begin() + index_end));
+    auto min_range = std::min(MAX_RANGE, *std::min_element(lidar->ranges.begin() + index_start, lidar->ranges.begin() + index_end));
+    ROS_ASSERT_MSG(min_range >= 0, "The minimal distance between the car and a potential obstacle is below zero.");
 
-    return min_dist < RANGE_THRESHOLD;
+    return min_range < RANGE_THRESHOLD;
 }
 
 void EmergencyStop::lidarCallback(const sensor_msgs::LaserScan::ConstPtr& lidar)
