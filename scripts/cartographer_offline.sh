93ce350371c7efd82d7a6837439a108f41f4832b
Nils <nilsjung42@gmail.com>
2019-04-17 16:48:53 +0200

    Cartographer scripts now produce logfiles

--- scripts/cartographer_offline.sh
+++ scripts/cartographer_offline.sh
@@ -4,19 +4,28 @@
 #   Must be run in the ros.package directory
 #
 currentdir=$(pwd)
-echo -n "Creating .urdf file..."
-xacro -o $currentdir/ros_ws/src/navigation_stack/car_cartographer/files/racer.urdf --inorder $currentdir/ros_ws/src/simulation/racer_description/urdf/racer.xacro use_gpu:=true visualize_lidar:=true >/dev/null 2>/dev/null
-echo " Done."
+logpath=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/cartographer.log
+
+echo -n "Creating .urdf file... " | tee $logpath
+echo "" >> $logpath
+xacro -o $currentdir/ros_ws/src/navigation_stack/car_cartographer/files/racer.urdf --inorder $currentdir/ros_ws/src/simulation/racer_description/urdf/racer.xacro use_gpu:=true visualize_lidar:=true 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
+echo "Done." | tee -a $logpath
+
 if [ $ROS_DISTRO == "kinetic" ] ; then
-    echo -n "Create map..."
-    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
-    echo " Done."
+    echo -n "Create map... " | tee -a $logpath
+    echo "" >> $logpath
+    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
+    echo "Done." | tee -a $logpath
 fi
+
 if [ $ROS_DISTRO == "melodic" ] ; then
-    echo -n "Create .pbstream..."
-    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO >/dev/null 2>/dev/null
-    echo " Done."
-    echo -n "Create map..."
-    roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$1 pose_graph_filename:=$1.pbstream >/dev/null 2>/dev/null
-    echo " Done."
+    echo -n "Create .pbstream... " | tee -a $logpath
+    echo "" >> $logpath
+    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$1 ros_version:=$ROS_DISTRO 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
+    echo "Done." | tee -a $logpath
+
+    echo -n "Create map... " | tee -a $logpath
+    echo "" >> $logpath
+    roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$1 pose_graph_filename:=$1.pbstream 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
+    echo "Done." | tee -a $logpath
 fi
