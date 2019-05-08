bc7207f83833a36db2170eb9bcdb48dace783ad7
Marian <mail@marian42.de>
2019-05-08 07:27:12 +0200

    Add track based reset options to reset_car.py

--- ros_ws/src/simulation/simulation_tools/script/reset_car.py
+++ ros_ws/src/simulation/simulation_tools/script/reset_car.py
@@ -6,13 +6,16 @@ from gazebo_msgs.msg import ModelState, ModelStates
 from tf.transformations import quaternion_from_euler
 
 import math
+import random
+
+from track import track, Point
 
 
-def reset(position, orientation):
+def set_pose(position, orientation):
     state = ModelState()
     state.model_name = "racer"
-    state.pose.position.x = position[0]
-    state.pose.position.y = position[1]
+    state.pose.position.x = position.x
+    state.pose.position.y = position.y
     state.pose.position.z = 0
 
     q = quaternion_from_euler(orientation, math.pi, 0)
@@ -24,8 +27,22 @@ def reset(position, orientation):
     set_model_state(state)
 
 
+def reset(progress=0, angle=0, offset_from_center=0, forward=True):
+    position = track.get_position(progress * track.length, offset_from_center)
+    angle += position.angle
+    if forward:
+        angle += math.pi
+    set_pose(position.point, angle)
+
+
+def reset_random(max_angle=0, max_offset_from_center=0, forward=True):
+    reset(random.random(), (random.random() * 2 - 1) * max_angle,
+          (random.random() * 2 - 1) * max_offset_from_center, forward)
+
+
 rospy.init_node("reset_car")
 rospy.wait_for_service('/gazebo/set_model_state')
 set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
-reset((0, 0), math.pi)
+if __name__ == "__main__":
+    set_pose(Point(0, 0), math.pi)
