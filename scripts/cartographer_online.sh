f595425974e70e7f71cc47289edf43f7fdecb764
Nils <nilsjung42@gmail.com>
2019-05-17 17:48:31 +0200

    WIP actual online script

--- scripts/cartographer_online.sh
+++ scripts/cartographer_online.sh
@@ -11,28 +11,21 @@ echo "" >> $logpath
 xacro -o $currentdir/ros_ws/src/navigation_stack/car_cartographer/files/racer.urdf --inorder $currentdir/ros_ws/src/simulation/racer_description/urdf/racer.xacro use_gpu:=true visualize_lidar:=true 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
 echo "Done." | tee -a $logpath
 
-echo "Recording topics... " | tee -a $logpath
-rosbag record -j -O ros_ws/src/navigation_stack/car_cartographer/files/recording.bag scan imu __name:=rosbag_recording 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath &
+echo "Recording topics & building map... " | tee -a $logpath
+rosbag record -j -O ros_ws/src/navigation_stack/car_cartographer/files/recording.bag scan imu tf __name:=rosbag_recording 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath &
+roslaunch car_cartographer cartographer_online.launch ros_version:=$ROS_DISTRO 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath &
 echo "Please drive with the car now (1.5 laps minimum, 3-5 laps recommended)."
 read -p "Press RETURN to stop recording."
+rosservice call /finish_trajectory 0 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
+rosservice call /write_state "{filename: '$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag.pbstream'}" | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
 rosnode kill /rosbag_recording 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
+rosnode kill /cartographer_node 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
+rosnode kill /rviz_cartographer 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
 wait
 echo "Stopped recording. Done." | tee -a $logpath
 
-if [ $ROS_DISTRO == "kinetic" ] ; then
-    echo -n "Create map... " | tee -a $logpath
-    echo "" >> $logpath
-    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag ros_version:=$ROS_DISTRO 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
-    echo "Done." | tee -a $logpath
-fi
-
 if [ $ROS_DISTRO == "melodic" ] ; then
-    echo -n "Create .pbstream... " | tee -a $logpath
-    echo "" >> $logpath
-    roslaunch car_cartographer cartographer_offline_fast.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag ros_version:=$ROS_DISTRO 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
-    echo "Done." | tee -a $logpath
-
-    echo -n "Create map... " | tee -a $logpath
+    echo -n "Write map... " | tee -a $logpath
     echo "" >> $logpath
     roslaunch car_cartographer cartographer_make_map.launch bag_filenames:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag pose_graph_filename:=$currentdir/ros_ws/src/navigation_stack/car_cartographer/files/recording.bag.pbstream 2>&1 | sed -r "s/([[:cntrl:]]\[[0-9]{1,3}m)|(..;?)|//g" >> $logpath
     echo "Done." | tee -a $logpath
