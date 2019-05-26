55b99882b253aff7867ea5278c311af1858b7b77
Nils <nilsjung42@gmail.com>
2019-05-26 21:04:40 +0200

    Fix travis consistency check error

--- scripts/travis/check-consistency.sh
+++ scripts/travis/check-consistency.sh
@@ -5,5 +5,5 @@ source $pathofscript/../../ros_ws/devel/setup.bash
 
 mv $pathofvescsim/config/car_config.yaml $pathofvescsim/config/car_config_copy.yaml || { echo 'Could not find "car_config.yaml" in package "vesc_sim".' ; exit 1; }
 rosrun vesc_sim params_to_yaml
-cmp -s $pathofvescsim/config/car_config.yaml $pathofvescsim/config/car_config_copy.yaml || { echo 'Inconsisteny between "vesc_sim/include/car_config.h" and "vesc_sim/config/car_config.yaml" detected. Please run "rosrun vesc_sim params_to_yaml" to fix this.' ; exit 0; }
+cmp -s $pathofvescsim/config/car_config.yaml $pathofvescsim/config/car_config_copy.yaml || { echo 'Inconsisteny between "vesc_sim/include/car_config.h" and "vesc_sim/config/car_config.yaml" detected. Please run "rosrun vesc_sim params_to_yaml" to fix this.' ; exit 1; }
 rm $pathofvescsim/config/car_config_copy.yaml
