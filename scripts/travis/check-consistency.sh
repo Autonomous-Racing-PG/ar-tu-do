3ef5ebe78987bfb7017439b9fdd0602eda1eddf5
Nils <nilsjung42@gmail.com>
2019-05-26 16:21:17 +0200

    Add vesc_to_odom node

--- scripts/travis/check-consistency.sh
+++ scripts/travis/check-consistency.sh
@@ -1,2 +1,9 @@
 pathofscript=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo $pathofscript
+pathofvescsim=$pathofscript/../../ros_ws/src/simulation/vesc_sim
+
+source $pathofscript/../../ros_ws/devel/setup.bash
+
+mv $pathofvescsim/config/car_config.yaml $pathofvescsim/config/car_config_copy.yaml || { echo 'Could not find "car_config.yaml" in package "vesc_sim".' ; exit 1; }
+rosrun vesc_sim params_to_yaml
+cmp -s $pathofvescsim/config/car_config.yaml $pathofvescsim/config/car_config_copy.yaml || { echo 'Inconsisteny between "vesc_sim/include/car_config.h" and "vesc_sim/config/car_config.yaml" detected. Please run "rosrun vesc_sim params_to_yaml" to fix this.' ; exit 0; }
+rm $pathofvescsim/config/car_config_copy.yaml
