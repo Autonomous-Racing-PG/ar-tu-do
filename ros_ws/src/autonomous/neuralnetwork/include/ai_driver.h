65c16e260d6c134caf898846601bd7de81a73118
Sami <sami.ansawi@tu-dortmund.de>
2019-06-17 14:26:56 +0200

    removed comments

--- ros_ws/src/autonomous/neuralnetwork/include/ai_driver.h
+++ ros_ws/src/autonomous/neuralnetwork/include/ai_driver.h
@@ -1,6 +1,5 @@
 #pragma once
 
-// http://leenissen.dk/fann/html/files/fann_cpp-h.html
 // clang-format off
 #include "floatfann.h"
 #include "fann_cpp.h"
--- ros_ws/src/autonomous/neuralnetwork/include/ai_util.h
+++ ros_ws/src/autonomous/neuralnetwork/include/ai_util.h
@@ -2,7 +2,6 @@
 
 #include "ai_math.h"
 
-// http://leenissen.dk/fann/html/files/fann_cpp-h.html
 // clang-format off
 #include "floatfann.h"
 #include "fann_cpp.h"
