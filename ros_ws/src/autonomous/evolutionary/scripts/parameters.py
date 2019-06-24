e820e62573e8b175953af7dea9120b8315a95fe7
Marian <marian.kleineberg@tu-dortmund.de>
2019-06-24 14:51:20 +0200

    Implement an evolutionary algorithm to drive the car

---
+++ ros_ws/src/autonomous/evolutionary/scripts/parameters.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+import osimport rospy
+
+from rospkg import RosPack
+from drive_msgs.msg import drive_param
+
+
+# TOPICS
+TOPIC_DRIVE_PARAMETERS = "/input/drive_param/autonomous"
+TOPIC_SCAN = "/scan"
+TOPIC_CRASH = "/crash"
+TOPIC_GAZEBO_MODEL_STATE = "/gazebo/model_states"
+
+STATE_SIZE = 8
+
+MIN_SPEED = 0.2
+MAX_SPEED = 0.4
+
+POPULATION_SIZE = 10
+SURVIVOR_COUNT = 4
+
+LEARN_RATE = 1
+normal_distribution = torch.distributions.normal.Normal(0, LEARN_RATE)
+
+drive_parameters_publisher = rospy.Publisher(
+    TOPIC_DRIVE_PARAMETERS, drive_param, queue_size=1)
+
+class NeuralCarDriver(nn.Module):
+    def __init__(self):
+        super(NeuralCarDriver, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(STATE_SIZE, 8),
+            nn.ReLU(),
+            nn.Linear(8, 8),
+            nn.ReLU(),
+            nn.Linear(8, 2),
+            nn.Tanh()
+        )
+
+        self.fitness = None
+
+    def drive(self, state):
+        action = self.layers.forward(state)
+        
+        message = drive_param()
+        message.angle = action[0].item()
+        message.velocity = (MIN_SPEED + MAX_SPEED) / 2 + action[1].item() * (MAX_SPEED - MIN_SPEED) / 2
+        drive_parameters_publisher.publish(message)
+
+    def to_vector(self):
+        state_dict = self.layers.state_dict()
+        tensors = [state_dict[key] for key in sorted(state_dict.keys())]
+        tensors = [torch.flatten(tensor) for tensor in tensors]
+        vector = torch.cat(tensors)
+
+        return vector
+
+    def load_vector(self, vector):
+        state_dict = self.layers.state_dict()
+        
+        position = 0
+        for key in sorted(state_dict.keys()):
+            old_value = state_dict[key]
+            size = np.prod(old_value.shape)
+            new_value = vector[position:position + size].reshape(old_value.shape)
+            state_dict[key] = new_value
+            position += size
+        self.layers.load_state_dict(state_dict)
+
+    def mutate(self):
+        parameters = self.to_vector()
+        parameters += normal_distribution.sample(parameters.shape)
+        offspring = NeuralCarDriver()
+        offspring.load_vector(parameters)
+        return offspring
---
+++ ros_ws/src/autonomous/evolutionary/scripts/train.py
@@ -0,0 +1,92 @@
+import math
+import rospy
+from sensor_msgs.msg import LaserScan
+from drive_msgs.msg import drive_param
+import random
+from std_msgs.msg import Empty
+from gazebo_msgs.msg import ModelStates
+import torch
+from parameters import *
+import simulation_tools.reset_car as reset_car
+
+
+class TrainingNode():
+    def __init__(self):
+        self.scan_indices = None
+        self.drive_parameters_publisher = rospy.Publisher(
+            TOPIC_DRIVE_PARAMETERS, drive_param, queue_size=1)
+        rospy.Subscriber(TOPIC_SCAN, LaserScan, self.on_receive_laser_scan)
+        rospy.Subscriber(TOPIC_CRASH, Empty, self.on_crash)
+
+        self.population = []
+        self.untested_population = [NeuralCarDriver() for _ in range(POPULATION_SIZE)]
+        self.current_driver = self.untested_population[0]
+
+        self.is_terminal_step = False
+        self.episode_length = 0
+        self.generation = 0
+        self.test = 0
+    
+    def convert_laser_message_to_tensor(self, message):
+        if self.scan_indices is None:
+            global scan_indices
+            count = (message.angle_max - message.angle_min) / \
+                message.angle_increment
+            scan_indices = [int(i * count / STATE_SIZE) for i in range(STATE_SIZE)]
+
+        values = [message.ranges[i] for i in scan_indices]
+        values = [v if not math.isinf(v) else 100 for v in values]
+        return torch.tensor(values, dtype=torch.float)
+
+    def on_receive_laser_scan(self, message):
+        state = self.convert_laser_message_to_tensor(message)
+        self.current_driver.drive(state)
+        self.episode_length += 1
+
+        if self.is_terminal_step:
+            self.on_complete_test()
+
+    def get_fitness(self):
+        return self.episode_length
+
+    def on_complete_test(self):
+        self.current_driver.fitness = self.get_fitness()
+
+        rospy.loginfo("Generation: {0}, Test: {1}, Fitness: {2}".format(self.generation, self.test, self.current_driver.fitness))
+        self.population.append(self.current_driver)
+        self.untested_population.remove(self.current_driver)
+        self.episode_length = 0
+        self.is_terminal_step = False
+
+        if len(self.untested_population) == 0:
+            self.current_driver = None
+            self.on_complete_generation()
+        else:
+            self.current_driver = self.untested_population[0]
+        self.test += 1
+
+        reset_car.reset()
+    
+    def on_complete_generation(self):
+        self.population.sort(key=lambda driver: driver.fitness, reverse=True)
+        self.population = self.population[:SURVIVOR_COUNT]
+
+        self.untested_population = list(self.population)
+        for _ in range(POPULATION_SIZE - SURVIVOR_COUNT):
+            parent = random.choice(self.population)
+            offspring = parent.mutate()
+            self.untested_population.append(offspring)
+        
+        self.population = []
+        self.current_driver = self.untested_population[0]
+        self.generation += 1
+
+    def on_crash(self, _):
+        if self.episode_length > 5:
+            self.is_terminal_step = True
+
+
+rospy.init_node('evolutionary_training', anonymous=True)
+reset_car.register_service()
+node = TrainingNode()
+rospy.spin()
