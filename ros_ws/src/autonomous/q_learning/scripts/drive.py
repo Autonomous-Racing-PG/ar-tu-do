972440337c71d25cb9b15c98dfa6492dfc588add
malikatamne <malik.atamne@tu-dortmund.de>
2019-07-24 20:15:53 +0200

    Created a new launch file for the policy gradient approach.

--- ros_ws/src/autonomous/q_learning/scripts/drive.py
+++ ros_ws/src/autonomous/q_learning/scripts/drive.py
@@ -3,7 +3,7 @@
 from qlearning import QLearningNode
 import os
 import rospy
-from parameters import MODEL_FILENAME
+from parametersPG import MODEL_FILENAME
 import torch
 
 
--- ros_ws/src/autonomous/q_learning/scripts/parameters.py
+++ ros_ws/src/autonomous/q_learning/scripts/parameters.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 import os
 import rospy
@@ -16,15 +17,52 @@ TOPIC_EPISODE_RESULT = "/qlearning/episode_result"
 
 # General parameters
 
-ACTIONS = [(-0.6, 0.2), (0.6, 0.2), (0, 0.2)]
+ACTIONS = [(-0.8, 0.1), (0.8, 0.1), (0.5, 0.2),
+           (-0.5, 0.2), (0, 0.2), (0, 0.4)]
 ACTION_COUNT = len(ACTIONS)
 
 # Only use some of the LIDAR measurements
 # When changing this value, also update laser_sample_count in qlearning.launch
-LASER_SAMPLE_COUNT = 32
+LASER_SAMPLE_COUNT = 8
 
 
 MODEL_FILENAME = os.path.join(RosPack().get_path("q_learning"), "model.to")
+
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+
+        self.fc1 = nn.Linear(LASER_SAMPLE_COUNT, 32)
+        self.fc2 = nn.Linear(32, 18)
+        self.fc3 = nn.Linear(18, ACTION_COUNT)
+
+        # Episode policy and reward history
+        self.policy_history = Variable(torch.Tensor())
+        self.reward_episode = []
+
+        # Overall reward and loss history
+        self.reward_history = []
+        self.loss_history = []
+
+    def forward(self, x):
+        model = torch.nn.Sequential(
+            self.fc1,
+            nn.ReLU(),
+            self.fc2,
+            nn.ReLU(),
+            self.fc3,
+            nn.Softmax(dim=-1)
+        )
+        return model(x)
+
+    def load(self):
+        if os.path.isfile(MODEL_FILENAME):
+            self.load_state_dict(torch.load(MODEL_FILENAME))
+            rospy.loginfo("Model parameters loaded.")
+
+    def save(self):
+        torch.save(self.state_dict(), MODEL_FILENAME)
 
 
 class NeuralQEstimator(nn.Module):
@@ -55,13 +93,14 @@ USE_EXISTING_PARAMETERS = False
 
 DISCOUNT_FACTOR = 0.99  # aka gamma
 
-MAX_EPISODE_LENGTH = 500
+MAX_EPISODE_LENGTH = 5000
 # Sample neural net update batch from the replay memory.
 # It contains this many steps.
 MEMORY_SIZE = 5000
 
 BATCH_SIZE = 128
 LEARNING_RATE = 0.0001
+LEARNING_RATE_POLICY_GRADIENT = 0.001
 
 # Probability to select a random episode starts at EPS_START
 # and reaches EPS_END once EPS_DECAY episodes are completed.
