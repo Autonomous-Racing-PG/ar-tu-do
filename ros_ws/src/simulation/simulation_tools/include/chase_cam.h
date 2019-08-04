aea5a97c7c5a963aaae01f6644fb67e6f782b82d
Marian <mail@marian42.de>
2019-08-04 21:47:54 +0200

    Clean up code

--- ros_ws/src/simulation/simulation_tools/include/chase_cam.h
+++ ros_ws/src/simulation/simulation_tools/include/chase_cam.h
@@ -1,23 +1,13 @@
 #pragma once
 
-#include <gazebo/gazebo.hh>
 #include <gazebo/gazebo_client.hh>
-#include <gazebo/msgs/msgs.hh>
 #include <gazebo/transport/transport.hh>
-#include <gazebo/transport/TransportTypes.hh>
 
-#include <ros/ros.h>
-#include <std_msgs/Empty.h>
-
-constexpr const char* TOPIC_CAMERA_POSE =
-    "/gazebo/racetrack/user_camera/joy_pose";
-
-constexpr const char* TOPIC_GAZEBO_POSES =
-    "/gazebo/racetrack/pose/info";
+constexpr const char* TOPIC_CAMERA_POSE = "/gazebo/racetrack/user_camera/joy_pose";
+constexpr const char* TOPIC_GAZEBO_POSES = "/gazebo/racetrack/pose/info";
+const std::string CAR_NAME = std::string("racer");
 
 const ignition::math::Vector3d CAMERA_OFFSET(-1.7, 0, 0.5);
-
-const std::string CAR_NAME = std::string("racer");
 
 /**
  * @brief Chase camera Gazebo node that listens to racer pose messages
@@ -30,9 +20,6 @@ class ChaseCam
     ChaseCam();
 
     private:
-    ros::NodeHandle m_ros_node_handle;
-    ros::Publisher m_crash_publisher;
-
     gazebo::transport::NodePtr m_gazebo_node;
     gazebo::transport::PublisherPtr m_camera_pose_publisher;
     gazebo::transport::SubscriberPtr m_racer_pose_subscriber;
--- ros_ws/src/simulation/simulation_tools/src/chase_cam.cpp
+++ ros_ws/src/simulation/simulation_tools/src/chase_cam.cpp
@@ -1,19 +1,12 @@
 #include "chase_cam.h"
 
-
-ignition::math::Pose3d convertPoseMessageToPose(const gazebo::msgs::Pose pose_message) {
+ignition::math::Pose3d convertPoseMessageToPose(const gazebo::msgs::Pose pose_message)
+{
     auto position = pose_message.position();
     auto orientation = pose_message.orientation();
 
-    return ignition::math::Pose3d(
-        position.x(),
-        position.y(),
-        position.z(),
-        orientation.w(),
-        orientation.x(),
-        orientation.y(),
-        orientation.z()
-    );
+    return ignition::math::Pose3d(position.x(), position.y(), position.z(), orientation.w(), orientation.x(),
+                                  orientation.y(), orientation.z());
 }
 
 ChaseCam::ChaseCam()
@@ -26,10 +19,13 @@ ChaseCam::ChaseCam()
         this->m_gazebo_node->Subscribe(TOPIC_GAZEBO_POSES, &ChaseCam::gazeboPosesCallback, this);
 }
 
-void ChaseCam::gazeboPosesCallback(ConstPosesStampedPtr& message) {
-    for (int i = 0; i < message->pose_size(); i++) {
+void ChaseCam::gazeboPosesCallback(ConstPosesStampedPtr& message)
+{
+    for (int i = 0; i < message->pose_size(); i++)
+    {
         auto pose_message = message->pose(i);
-        if (pose_message.name() == CAR_NAME) {
+        if (pose_message.name() == CAR_NAME)
+        {
             auto pose = convertPoseMessageToPose(pose_message);
             this->updateCamera(pose);
             return;
@@ -37,7 +33,8 @@ void ChaseCam::gazeboPosesCallback(ConstPosesStampedPtr& message) {
     }
 }
 
-void ChaseCam::updateCamera(ignition::math::Pose3d& car_pose) {
+void ChaseCam::updateCamera(ignition::math::Pose3d& car_pose)
+{
     auto car_rotation = ignition::math::Quaterniond(0, 0, car_pose.Rot().Yaw());
 
     this->m_last_position = car_pose.Pos() * 0.1 + this->m_last_position * 0.9;
@@ -45,11 +42,12 @@ void ChaseCam::updateCamera(ignition::math::Pose3d& car_pose) {
 
     auto camera_position = this->m_last_position + this->m_last_rotation * CAMERA_OFFSET;
 
-    auto camera_pose = ignition::math::Pose3d(camera_position, this->m_last_rotation);    
+    auto camera_pose = ignition::math::Pose3d(camera_position, this->m_last_rotation);
     this->publishCameraPose(camera_pose);
 }
 
-void ChaseCam::publishCameraPose(ignition::math::Pose3d& pose) {
+void ChaseCam::publishCameraPose(ignition::math::Pose3d& pose)
+{
     gazebo::msgs::Pose message;
     gazebo::msgs::Set(&message, pose);
     this->m_camera_pose_publisher->Publish(message);
@@ -57,12 +55,13 @@ void ChaseCam::publishCameraPose(ignition::math::Pose3d& pose) {
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "chase_cam");
     gazebo::client::setup(argc, argv);
-
     ChaseCam chase_cam;
 
-    ros::spin();
+    while (true)
+    {
+        gazebo::common::Time::MSleep(1000);
+    }
 
     gazebo::client::shutdown();
     return EXIT_SUCCESS;
